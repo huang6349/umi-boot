49d454c548b3d9a90a9c1e22edc58412607e3eff
huang6349 <252683858@qq.com>
2019-11-28 12:14:58 +0800

    :wrench::art: 修改配置文件并代码格式

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const IndexPage = () => {
-  return <React.Fragment />;
+  return <React.Fragment>Hello, Basic Project Template</React.Fragment>;
 };
 
 export default IndexPage;
